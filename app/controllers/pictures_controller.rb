5a76083ec0f5dd84d64e71cf46b6e90fb1131718
Kyndrea Head <khead326@users.noreply.github.com>
2017-04-28 10:10:07 -0500

    in class progress

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,12 +7,30 @@ class PicturesController < ApplicationController
     render("pic_templates/create_row.html.erb")
   end
 
-def index
-  render("pic_templates/index.html.erb")
-end
+  def index
+    render("pic_templates/index.html.erb")
+  end
 
-def show
-  render("pic_templates/show.html.erb")
-end
+  def show
+    # the parameters look like: {"the_id"=>"4"}
+id_number=params["the_id"]
+p=Photo.find(id_number)
+@the_caption=p.caption
+@the_source=p.source
+@the_timestamp=p.created_at
 
+    render("pic_templates/show.html.erb")
+  end
+
+  def edit_form
+    render("pic_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    render("pic_templates/update_row.html.erb")
+  end
+
+  def destroy_row
+    render("pic_templates/destroy_row.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # CREATE
   get("/photos/new", { :controller => "pictures", :action => "new_form" })
   get("/create_photo", { :controller => "pictures", :action => "create_row" })
@@ -14,5 +13,4 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_photo/:da_id", { :controller => "pictures", :action => "destroy_row" })
-  
 end
